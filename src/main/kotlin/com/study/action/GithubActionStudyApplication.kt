c7fdca2ca385f5c49f09ed946e5a2443840590f7
behappyleee <dhfhfkwhdk@naver.com>
2024-07-21 15:53:48 +0900

    studying commit

---
+++ src/main/kotlin/com/study/action/GithubActionStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.study.action
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<GithubActionStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/action/GithubActionStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.action
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
